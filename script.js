44031eb1955e8f08594e85ecf07af6f090f69aee
Thomas Gakumu <gakumuthomas@gmail.com>
2026-06-15 15:30:45 +0200

    93' . 21:00

--- script.js
+++ script.js
@@ -58,15 +58,44 @@ class App {
     inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
   }
   _newWorkout(e) {
+    const validInputs = (...inputs) =>
+      inputs.every(inp => Number.isFinite(inp));
     e.preventDefault();
-    //Clear input fields
-    inputDistance.value =
-      inputDuration.value =
-      inputCadence.value =
-      inputElevation.value =
-        '';
+    //Get data from form
+    const type = inputType.value;
+    const distance = inputDistance.value;
+    const duration = inputDuration.value;
 
-    //Display marker
+    //If workout running,create running object
+    if (type === 'running') {
+      const cadence = +inputCadence.value;
+      const allPositive = (...inputs) => inputs.every(inp > 0);
+      //Check if data is valid
+      if (
+        // !Number.isFinite(distance) ||
+        // !Number.isFinite(duration) ||
+        // !Number.isFinite(cadence)
+        !validInputs(distance, duration, cadence) ||
+        !allPositive(distance, duration, cadence)
+      )
+        return alert('Input has to be positive numbers');
+      const workout = new Running();
+    }
+
+    //If workout cycling,create cycling object
+    if (type === 'cycling') {
+      const elevation = inputElevation.value;
+      if (
+        !validInputs(distance, duration, elevation) ||
+        !allPositive(distance, duration)
+      )
+        return alert('Input has to be positive numbers');
+    }
+
+    //Add new object to workout array
+
+    //Render workout on map as marker
+
     const { lat, lng } = this.#mapEvent.latlng;
     L.marker([lat, lng])
       .addTo(this.#map)
@@ -82,6 +111,16 @@ class App {
       .setPopupContent('Workout')
 
       .openPopup();
+
+    //Render workout on list
+
+    //Hide form + clear input fields
+
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
   }
 }
 const app = new App();
